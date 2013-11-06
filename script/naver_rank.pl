5adb7e2d1eeb00702383e6a75853dade2e48c45d
YunChang Kang <codenewb@gmail.com>
2013-11-07 00:22:40 +0900

    naver top10 pasing & log 파일생성 완료

--- script/naver_rank.pl
+++ script/naver_rank.pl
@@ -9,10 +9,21 @@ my $rank_url = 'http://openapi.naver.com/search?key=b7d595844816c0b937e204837a33
 
 my $ua = LWP::UserAgent->new;
 my $resp = $ua->get($rank_url);
+my $decode_body;
 
 if ($resp->is_success) {
-    print $resp->decoded_content;
+     $decode_body = $resp->decoded_content;
 }
 else {
     die $resp->status_line;
 }
+
+my @top_tens = $decode_body =~ m{<K>(.*?)</K>}gsm; 
+my $file = "top_ten.log";
+
+open(my $fh, ">", $file) or die "cannot open > top_ten.log: $!"; 
+
+foreach my $p ( @top_tens ) {
+    print $fh "$p\n";
+}
+close $fh;
